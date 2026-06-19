e5f17dd375a87c6f9f6d8a9e7934834f2bb8b89c
Florian Torres <50424335+torresflo@users.noreply.github.com>
2026-06-19 22:07:46 +0200

    Implement timer behavior.

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,9 +1,88 @@
 #include "Timer.h"
 
-void Timer::initialize()
+#include <format>
+
+#include "CentralProcessingUnit.h"
+#include "MathUtils.h"
+
+void HardwareTimer::initialize()
 {
+    dividerRegister = 0xAC00;
 }
 
-void Timer::tick()
+void HardwareTimer::tick()
 {
+    u16 previousDiv = dividerRegister;
+    dividerRegister++;
+
+    bool timerUpdate = false;
+    switch(timerControl & 0b11)
+    {
+        case 0b00:
+            timerUpdate = MathUtils<u16>::getBitValue(previousDiv, 9) && !MathUtils<u16>::getBitValue(dividerRegister, 9);
+            break;
+        case 0b01:
+            timerUpdate = MathUtils<u16>::getBitValue(previousDiv, 3) && !MathUtils<u16>::getBitValue(dividerRegister, 3);
+            break;
+        case 0b10:
+            timerUpdate = MathUtils<u16>::getBitValue(previousDiv, 5) && !MathUtils<u16>::getBitValue(dividerRegister, 5);
+            break;
+        case 0b11:
+            timerUpdate = MathUtils<u16>::getBitValue(previousDiv, 7) && !MathUtils<u16>::getBitValue(dividerRegister, 7);
+            break;
+    }
+
+    if(timerUpdate && MathUtils<u8>::getBitValue(timerControl, 2))
+    {
+        timerCounter++;
+        if(timerCounter == 0xFF)
+        {
+            timerCounter = timerModulo;
+            CPU->requestInterrupt(InterruptType::Timer);
+        }
+    }
+}
+
+void HardwareTimer::writeTimer(u16 address, u8 value)
+{
+    switch(address)
+    {
+        case 0xFF04:
+            dividerRegister = 0;
+            break;
+        case 0xFF05:
+            timerCounter = value;
+            break;
+        case 0xFF06:
+            timerModulo = value;
+            break;
+        case 0xFF07:
+            timerControl = value;
+            break;
+    }
+}
+
+u8 HardwareTimer::readTimer(u16 address) const
+{
+    switch(address)
+    {
+        case 0xFF04:
+            return dividerRegister >> 8;
+        case 0xFF05:
+            return timerCounter;
+        case 0xFF06:
+            return timerModulo;
+        case 0xFF07:
+            return timerControl;
+    }
+
+    Log::print(LogLevel::Error, std::format("Unsupported timer reading (0x{:4X}).", address));
+    return 0;
+}
+
+void HardwareTimer::initialize(CentralProcessingUnit* cpuPtr)
+{
+    this->CPU = cpuPtr;
+    
+    dividerRegister = 0xABCC;
 }
--- src/Timer.h
+++ src/Timer.h
@@ -1,8 +1,25 @@
 #pragma once
 
-class Timer
+#include "Common.h"
+
+class CentralProcessingUnit;
+
+class HardwareTimer
 {
 public:
     void initialize();
     void tick();
+
+    void writeTimer(u16 address, u8 value);
+    u8 readTimer(u16 address) const;
+
+    void initialize(CentralProcessingUnit* cpuPtr);
+
+private:
+    u16 dividerRegister = 0; //DIV
+    u8 timerCounter = 0; //TIMA
+    u8 timerModulo = 0; //TMA
+    u8 timerControl = 0; //TAC
+
+    CentralProcessingUnit* CPU;
 };
